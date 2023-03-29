074ab54f5889984f74dc3b97ae2bfe4b00343d51
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-03-29 18:15:47 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post='강남 우동 맛집';
+  let [글제목, 글제목변경]=useState('남자 코드 추천');
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4 style={{color:'red'}}className="post-title">{post}</h4>
+      {/* <h4>{post}</h4> */}
+      <div className='list'>
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
